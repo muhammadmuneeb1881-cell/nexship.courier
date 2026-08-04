9986f17ee358eca75f18aa908df286868e29f9c0
Muneeb Ur rehman <muhammadmuneeb1881@gmail.com>
2026-08-03 17:09:39 -0700

    Add files via upload

---
+++ app/merchant/bulk-booking/page.tsx
@@ -0,0 +1,7 @@
+import MerchantBulkBookingForm from "../../../components/merchant/MerchantBulkBookingForm";
+
+export const dynamic = "force-dynamic";
+
+export default function MerchantBulkBookingPage() {
+  return <MerchantBulkBookingForm />;
+}
